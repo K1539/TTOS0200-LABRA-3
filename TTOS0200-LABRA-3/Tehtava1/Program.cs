cbd074db07f3e29afbf5dc5eed3de2fd14800948
K1539 <K1539@ICT-421-WS-37>
2017-01-16 13:30:12 +0200

    Tunnilla tehtyä

---
+++ TTOS0200-LABRA-3/Tehtava1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tehtava1;
+
+namespace Tehtava1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaaKiuas();
+        }
+        //Tehtävän 1 Kiuas-luokan testaus
+        static void TestaaKiuas()
+        {
+            //luodaan kiuas -olio
+            Kiuas kiuas = new Tehtava1.Kiuas();
+            //pistetään kiuas lämpenemään ja asetetaan lämpö & kosteutta
+            kiuas.OnkoPaalla = true;
+            kiuas.Lampotila = 90;
+            kiuas.Kosteus = 50;
+            //näytetään konsolilla
+            Console.WriteLine("Kiuas on päällä {0}", kiuas.OnkoPaalla);
+            Console.WriteLine("Kiukaan lämpötila on {0}", kiuas.Lampotila);
+            Console.WriteLine("Kiukaan kosteus {0}", kiuas.Kosteus);
+        }
+    }
+}
---
+++ TTOS0200-LABRA-3/Tehtava3/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Luo Vehicle-luokka seuraavien tietojen mukaisesti:
+
+ominaisuudet
+
+Name:string
+Speed:int
+Tyres:int
+toiminnot
+
+PrintData(), tulostaa Vehiclen ominaisuudet näytölle
+ToString():string, palauttaa kaikki Vehiclen ominaisuudet yhtenä merkkijonona
+Toteuta Vehicle-luokan ohjelmointi sekä pääohjelma, jolla luot olion Vehicle-luokasta. Muuta olion arvoja ja tulosta olion arvoja konsolille.
+*
+* Jere Liikka 16.1.2017
+* */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava3
+{
+    class Vehicle
+    {
+        public string Name;
+        public int Speed;
+        public int Tyres;
+
+        public void PrintData()
+        {
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle Auto = new Vehicle();
+            Auto.Name = "Volvo";
+            Auto.Speed = 420;
+            Auto.Tyres = 4;
+        }    
+    }
+}

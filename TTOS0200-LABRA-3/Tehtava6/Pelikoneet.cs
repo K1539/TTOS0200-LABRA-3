56fb87e991765fe331d0b7a12965df24dcb1773a
jerel <jerel@pytis>
2017-01-16 23:23:06 +0200

    teht 6 alku

---
+++ TTOS0200-LABRA-3/Tehtava6/Pelikoneet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class Pelikoneet
+    {
+        public string Nimi;
+        public int Julkaisuvuosi;
+
+    }
+}

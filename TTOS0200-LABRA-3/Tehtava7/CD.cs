53936c2cd23b8e5ebd8ed4301a32284aacda17ce
jerel <jerel@pytis>
2017-01-18 00:02:15 +0200

    t7

---
+++ TTOS0200-LABRA-3/Tehtava7/CD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class CD
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+        public string SongName { get; set; }
+        public CD(string songname)
+        {
+            SongName = songname;
+        }
+        public CD() {}
+    }
+}

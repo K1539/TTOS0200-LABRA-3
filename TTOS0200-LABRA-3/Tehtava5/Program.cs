4314e9c33c8f746c8f5071c26eca4d2dc4336e6a
jerel <jerel@pytis>
2017-01-17 22:57:19 +0200

    t6 kommentointi

--- TTOS0200-LABRA-3/Tehtava5/Program.cs
+++ TTOS0200-LABRA-3/Tehtava5/Program.cs
@@ -11,20 +11,21 @@ namespace JAMK.IT
     {
         static void Main(string[] args)
         {
+            //tehdään listaan lisättävät objektit
             Opiskelija Jere = new Opiskelija("Jere", 20, "TTV16S3");
             Opiskelija Juuso = new Opiskelija("Juuso", 22, "TTV16S1");
             Opiskelija Matti = new Opiskelija("Matti", 19, "TTV16S2");
             Opiskelija Liisa = new Opiskelija("Liisa", 20, "TTV16S4");
             Opiskelija Alina = new Opiskelija("Alina", 20, "TTV16S4");
 
-            List<Opiskelija> OpiskelijaLista = new List<Opiskelija>();
+            List<Opiskelija> OpiskelijaLista = new List<Opiskelija>(); // luo lista ja lisää objektit
             OpiskelijaLista.Add(Jere);
             OpiskelijaLista.Add(Juuso);
             OpiskelijaLista.Add(Liisa);
             OpiskelijaLista.Add(Matti);
             OpiskelijaLista.Add(Alina);
 
-            for (int i = 0; i < OpiskelijaLista.Count; i++)
+            for (int i = 0; i < OpiskelijaLista.Count; i++) // käydään kaikki listan jäsenet läpi
             {
                 Console.WriteLine("Opiskelija " + (i+1));
                 Console.WriteLine("   Nimi: " + OpiskelijaLista[i].Nimi);
